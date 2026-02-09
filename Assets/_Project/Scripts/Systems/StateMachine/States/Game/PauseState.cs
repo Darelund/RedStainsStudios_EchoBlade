b268a00cef4af4dc9c1eed6cdfe9086de5f0ba72
@Spexuz <FilipChristianPerneklo@Gmail.com>
2026-02-09 16:14:47 +0100

    Update PauseState.cs
    Disable Abilitybar and Questlog on PauseState Enter and Exit

--- Assets/_Project/Scripts/Systems/StateMachine/States/Game/PauseState.cs
+++ Assets/_Project/Scripts/Systems/StateMachine/States/Game/PauseState.cs
@@ -6,11 +6,15 @@ using UnityEngine.Rendering.Universal;
 public class PauseState : State
 {
     [SerializeField] private GameObject pauseScreenUI;
+    [SerializeField] private GameObject abilityBarUI; //added refs to hide elements during pause - filip
+    [SerializeField] private GameObject questScreenUI;
     public override void EnterState()
     {
         AIManager.Instance.StopAllAI();
       //  Time.timeScale = 0; //Navmesh agents would keep going without this, I could make it work by stopping all navmesh agents, but to lazy - Vidar
         pauseScreenUI.SetActive(true);
+        abilityBarUI.SetActive(false);
+        questScreenUI.SetActive(false);
         GameObject.FindAnyObjectByType<Volume>().profile.TryGet(out DepthOfField d);
         d.active = true;
     }
@@ -29,6 +33,8 @@ public class PauseState : State
         AIManager.Instance.ResumeAllAI();
        // Time.timeScale = 1;
         pauseScreenUI.SetActive(false);
+        abilityBarUI.SetActive(true);
+        questScreenUI.SetActive(true);
         GameObject.FindAnyObjectByType<Volume>().profile.TryGet(out DepthOfField d);
         d.active = false;
     }
