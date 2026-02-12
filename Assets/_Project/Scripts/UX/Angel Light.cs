95333a078068b164eef30e584c05db3a39da24b3
@Spexuz <FilipChristianPerneklo@Gmail.com>
2026-02-12 11:00:34 +0100

    Angel Light

---
+++ Assets/_Project/Scripts/UX/Angel Light.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
+public class AngelLight : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+    
+    IEnumerator ShowPrompt()
+    {
+        
+    }
+
+    IEnumerator HidePrompt()
+    {
+        
+    }
+
+}
+
